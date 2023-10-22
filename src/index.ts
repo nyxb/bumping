bac3066dbf41ee4313635ab23254958b2dfca74b
nyxb <contact@nyxb.email>
2023-10-22 16:17:55 +0200

    🔧 refactor: update import statements to use ES6 syntax for better readability and consistency 🔨 refactor: replace var with let and const for better scope control and predictability 🔧 refactor: remove unnecessary semicolons for cleaner code 🔨 refactor: replace double quotes with single quotes for string literals for consistency 🔧 refactor: replace function declarations with arrow functions for cleaner syntax 🔨 refactor: replace if-else statements with ternary operators for cleaner code 🔧 refactor: replace '==' with '===' for strict equality checking 🔨 refactor: replace '!=' with '!==' for strict inequality checking 🔧 refactor: replace 'new' keyword with direct function calls for cleaner code 🔨 refactor: replace 'null' with 'undefined' for better semantics 🔧 refactor: replace 'function' keyword with arrow functions for cleaner syntax 🔨 refactor: replace 'var' with 'let' and 'const' for better scope control and predictability 🔧 refactor: remove unnecessary semicolons for cleaner code 🔨 refactor: replace double quotes with single quotes for string literals for consistency 🔧 refactor: replace function declarations with arrow functions for cleaner syntax 🔨 refactor: replace if-else statements with ternary operators for cleaner code 🔧 refactor: replace '==' with '===' for strict equality checking 🔨 refactor: replace '!=' with '!==' for strict inequality checking 🔧 refactor: replace 'new' keyword with direct function calls for cleaner code 🔨 refactor: replace 'null' with 'undefined' for better semantics 🔧 refactor: replace 'function' keyword with arrow functions for cleaner syntax 🔨 refactor: replace 'var' with 'let' and 'const' for better scope control and predictability 🔧 refactor: remove unnecessary semicolons for cleaner code 🔨 refactor: replace double quotes with single quotes for string literals for consistency 🔧 refactor: replace function declarations with arrow functions for cleaner syntax 🔨 refactor: replace if-else statements with ternary operators for cleaner code 🔧 refactor: replace '==' with '===' for strict equality checking 🔨 refactor: replace '!=' with '!==' for strict inequality checking 🔧 refactor: replace 'new' keyword with direct function calls for cleaner code

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,11 @@
-import { versionBump } from "./version-bump";
+import { versionBump, versionBumpInfo } from './version-bump'
 
-// Exprot the external type definitions as named exports
-export { ReleaseType } from "./release-type";
-export * from "./types/version-bump-options";
-export * from "./types/version-bump-results";
-export * from "./types/version-bump-progress";
+export { ReleaseType } from './release-type'
+export * from './types/version-bump-options'
+export * from './types/version-bump-results'
+export * from './types/version-bump-progress'
 
-// Export `versionBump` as a named export and the default export
-export { versionBump };
-export default versionBump;
+export * from './config'
 
-// CommonJS default export hack
-if (typeof module === "object" && typeof module.exports === "object") {
-  module.exports = Object.assign(module.exports.default, module.exports);
-}
+export { versionBump, versionBumpInfo }
+export default versionBump
