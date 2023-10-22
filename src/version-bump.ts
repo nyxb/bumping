617981c74cdce91ec3de5f18d8d6c7277024db25
nyxb <contact@nyxb.email>
2023-10-22 16:18:55 +0200

    🔥 remove(cli/help.ts): remove redundant help.ts file as it's no longer needed 🔧 refactor(cli/exit-code.ts): add trailing comma to ExitCode enum for better git diff ✨ feat(version-bump.ts): add confirmation prompt before version bump ✨ feat(version-bump.ts): add option to execute script after version bump 🔧 refactor(version-bump.ts): refactor versionBump function to improve readability and maintainability 🆕 feat(version-bump.ts): add versionBumpInfo function to get version bump operation details

--- src/version-bump.ts
+++ src/version-bump.ts
@@ -1,19 +1,24 @@
-import { getNewVersion } from "./get-new-version";
-import { getOldVersion } from "./get-old-version";
-import { gitCommit, gitPush, gitTag } from "./git";
-import { Operation } from "./operation";
-import { runNpmScript } from "./run-npm-script";
-import { VersionBumpOptions } from "./types/version-bump-options";
-import { NpmScript } from "./types/version-bump-progress";
-import { VersionBumpResults } from "./types/version-bump-results";
-import { updateFiles } from "./update-files";
+import process from 'node:process'
+import * as ezSpawn from '@jsdevtools/ez-spawn'
+import c from 'picocolors'
+import symbols from 'log-symbols'
+import prompts from 'prompts'
+import { getNewVersion } from './get-new-version'
+import { getOldVersion } from './get-old-version'
+import { formatVersionString, gitCommit, gitPush, gitTag } from './git'
+import { Operation } from './operation'
+import { runNpmScript } from './run-npm-script'
+import type { VersionBumpOptions } from './types/version-bump-options'
+import { NpmScript } from './types/version-bump-progress'
+import type { VersionBumpResults } from './types/version-bump-results'
+import { updateFiles } from './update-files'
 
 /**
  * Prompts the user for a version number and updates package.json and package-lock.json.
  *
  * @returns - The new version number
  */
-export async function versionBump(): Promise<VersionBumpResults>;
+export async function versionBump(): Promise<VersionBumpResults>
 
 /**
  * Bumps the version number in package.json, package-lock.json.
@@ -25,47 +30,96 @@ export async function versionBump(): Promise<VersionBumpResults>;
  * - A release type (e.g. "major", "minor", "patch", "prerelease", etc.)
  * - "prompt" to prompt the user for the version number
  */
-export async function versionBump(release: string): Promise<VersionBumpResults>;
+export async function versionBump(release: string): Promise<VersionBumpResults>
 
 /**
  * Bumps the version number in one or more files, prompting the user if necessary.
  * Optionally also commits, tags, and pushes to git.
  */
-export async function versionBump(options: VersionBumpOptions): Promise<VersionBumpResults>;
+export async function versionBump(options: VersionBumpOptions): Promise<VersionBumpResults>
 
 /**
  * Bumps the version number in one or more files, prompting the user if necessary.
  * Optionally also commits, tags, and pushes to git.
  */
-export async function versionBump(arg: VersionBumpOptions | string = {}): Promise<VersionBumpResults> {
-  if (typeof arg === "string") {
-    arg = { release: arg };
-  }
+export async function versionBump(arg: VersionBumpOptions | string = {}): Promise<VersionBumpResults | undefined> {
+  if (typeof arg === 'string')
+    arg = { release: arg }
 
-  let operation = await Operation.start(arg);
+  const operation = await Operation.start(arg)
 
   // Get the old and new version numbers
-  await getOldVersion(operation);
-  await getNewVersion(operation);
+  await getOldVersion(operation)
+  await getNewVersion(operation)
+
+  if (arg.confirm) {
+    printSummary(operation)
+
+    if (!await prompts({
+      name: 'yes',
+      type: 'confirm',
+      message: 'Bump?',
+      initial: true,
+    }).then(r => r.yes))
+      process.exit(1)
+  }
 
   // Run npm preversion script, if any
-  await runNpmScript(NpmScript.PreVersion, operation);
+  await runNpmScript(NpmScript.PreVersion, operation)
 
   // Update the version number in all files
-  await updateFiles(operation);
+  await updateFiles(operation)
+
+  if (operation.options.execute) {
+    console.log(symbols.info, 'Executing script', operation.options.execute)
+    await ezSpawn.async(operation.options.execute, { stdio: 'inherit' })
+    console.log(symbols.success, 'Script finished')
+  }
 
   // Run npm version script, if any
-  await runNpmScript(NpmScript.Version, operation);
+  await runNpmScript(NpmScript.Version, operation)
 
   // Git commit and tag, if enabled
-  await gitCommit(operation);
-  await gitTag(operation);
+  await gitCommit(operation)
+  await gitTag(operation)
 
   // Run npm postversion script, if any
-  await runNpmScript(NpmScript.PostVersion, operation);
+  await runNpmScript(NpmScript.PostVersion, operation)
 
   // Push the git commit and tag, if enabled
-  await gitPush(operation);
+  await gitPush(operation)
 
-  return operation.results;
+  return operation.results
+}
+
+function printSummary(operation: Operation) {
+  console.log()
+  console.log(`   files ${operation.options.files.map(i => c.bold(i)).join('\n         ')}`)
+  if (operation.options.commit)
+    console.log(`  commit ${c.bold(formatVersionString(operation.options.commit.message, operation.state.newVersion))}`)
+  if (operation.options.tag)
+    console.log(`     tag ${c.bold(formatVersionString(operation.options.tag.name, operation.state.newVersion))}`)
+  if (operation.options.execute)
+    console.log(` execute ${c.bold(operation.options.execute)}`)
+  if (operation.options.push)
+    console.log(`    push ${c.cyan(c.bold('yes'))}`)
+  console.log()
+  console.log(`    from ${c.bold(operation.state.oldVersion)}`)
+  console.log(`      to ${c.green(c.bold(operation.state.newVersion))}`)
+  console.log()
+}
+
+/**
+ * Bumps the version number in one or more files, prompting users if necessary.
+ */
+export async function versionBumpInfo(arg: VersionBumpOptions | string = {}): Promise<Operation> {
+  if (typeof arg === 'string')
+    arg = { release: arg }
+
+  const operation = await Operation.start(arg)
+
+  // Get the old and new version numbers
+  await getOldVersion(operation)
+  await getNewVersion(operation)
+  return operation
 }
